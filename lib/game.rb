5f149c38fef53b69a847528ecef9e59ea2b02a58
kiizerd <ajsewell13@gmail.com>
2021-08-12 01:19:31 -0500

    included Display module

--- lib/game.rb
+++ lib/game.rb
@@ -7,12 +7,23 @@ require_relative 'connector'
 
 class Game
 
+  include Display
+
   def initialize
     @board     = GameBoard.new
-    @players   = [Human.new('X'), Player.new('O')]
     @connector = Connector.new(@board)
   end
 
+  def main_menu
+    welcome_screen
+    option = user_input(1, 3)
+    case option
+    when 1 then player_selection
+    when 2 then more_info
+    when 3 then exit(0)  
+    end
+  end
+  
   def start
     p1, p2  = @players
     current = p1
@@ -24,29 +35,62 @@ class Game
     game_over
   end
 
+  def restart
+    @board     = GameBoard.new
+    @connector = Connector.new(@board)
+    player_selection
+  end
+
+  def player_selection
+    puts `clear`, player_selection_prompt
+    human_players = user_input(0, 2)
+    case human_players
+    when 0 then @players = [Comp.new('O'), Comp.new('X')]      
+    when 1 then @players = [Human.new('X'), Comp.new('O')]
+    when 2 then @players = [Human.new('O'), Human.new('X')]
+    end
+
+    start
+  end
+
   def try_move player
-    col = player.move
+    show_board(@board.board)
+    col = player.is_a?(Human) ? user_input(1, 7) - 1 : player.move
     if @board.check_column col
-      @board.apply_move player 
+      @board.apply_move(col, player.shape)
       return true
     end
     return false
   end
   
-  def user_input(min, max, player=@players.first)
-    puts "Input a number between #{min} and #{max}"
-    input = gets.chomp.to_i
-    # TODO - Add a call to check input against some strings
-    # that correspond to commands for the game i.e - exit, restart, stats
-    input = gets.chomp.to_i until verify_input(min, max, input)
-    input
+  def user_input(min, max)
+    human_move_prompt(min, max)
+    loop do
+      input = gets.chomp.to_i
+      return input if verify_input(min, max, input)
+    end
   end
 
   def verify_input(min, max, input)
     input >= min && input <= max ? input : false
   end
   
-  def game_over(winner=nil)
-    puts "#{winner} wins"
+  def game_over
+    winner = check_for_winner
+    show_board(@board.board)
+    show_game_over(winner ? winner : 'No one')
+    option = user_input(1, 2)
+    case option
+    when 1 then restart
+    when 2 then exit(0)
+    end
+  end
+
+  def check_for_winner
+    return false if @connector.path.size < 4
+    root  = @connector.path[0]
+    shape = @board.board[-(root[0] + 1)][root[1]]
+
+    return @players.select { |p| p.shape == shape }[0]
   end
 end
